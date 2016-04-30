c56ea057a106ec985003bc7f7a78b62e333d537c
aochuih <aochuih@hotmail.com>
2016-04-30 20:53:57 +0800

    chapter 2

---
+++ app.js
@@ -0,0 +1,31 @@
+var koa = require('koa');
+var app = koa();
+
+var port = process.argv[2];
+
+app.use(function *(next) {
+    console.log('request:' + this.request.url);
+    if (this.path !== '/') {
+        return yield next;
+    }
+
+    this.body = 'hello koa';
+});
+
+app.use(function *(next) {
+    if (this.path !== '/404') {
+        return yield next;
+    }
+
+    this.body = 'page not found';
+});
+
+app.use(function *(next) {
+    if (this.path !== '/500') {
+        return yield next;
+    }
+
+    this.body = 'internal server error';
+});
+
+app.listen(port || 8080);
